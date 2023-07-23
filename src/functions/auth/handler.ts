da2adc70e93480b66835a64e9cdd33106494245b
Abdulquadri Adegbiji <adegbijilanre@gmail.com>
2023-07-23 18:55:24 +0100

    ....

--- src/functions/auth/handler.ts
+++ src/functions/auth/handler.ts
@@ -11,7 +11,7 @@ const JWT_SECRET = 'your-jdygegdy373ihuojw,dokow,dwumduygy3i38hdhebduehb-key'; /
 
 
 // Helper function to create a user in DynamoDB
-async function createUser(user: { email: string; password: string }): Promise<void> {
+async function createUser(user: { email: string; password: string; name:string; address:string }): Promise<void> {
   const params = {
     TableName: USERS_TABLE,
     Item: user,
@@ -23,23 +23,6 @@ async function createUser(user: { email: string; password: string }): Promise<vo
     console.error('Error creating user:', error);
   }
 }
-
-
-// // Helper function to get a user by email from DynamoDB
-// async function getUserByEmail(email: string): Promise<any | null> {
-//   const params = {
-//     TableName: USERS_TABLE,
-//     Key: { email },
-//   };
-
-//   try {
-//     const result = await dynamoDb.get(params).promise();
-//     return result.Item || null;
-//   } catch (error) {
-//     console.error('Error fetching user:', error);
-//     return null;
-//   }
-// }
 
 async function getUserByEmail(email: string): Promise<any | null> {
   const params = {
@@ -58,7 +41,9 @@ async function getUserByEmail(email: string): Promise<any | null> {
 
 interface SignupRequestBody {
   email: string;
+  name: string;
   password: string;
+  address: string;
 }
 
 interface LoginRequestBody {
@@ -73,6 +58,7 @@ export const signup: APIGatewayProxyHandler = async (event) => {
 
     // Check if the user already exists in DynamoDB
     const existingUser = await getUserByEmail(requestBody.email);
+    console.log("user is here", existingUser);
     if (existingUser) {
       return {
         statusCode: 409,
@@ -84,10 +70,10 @@ export const signup: APIGatewayProxyHandler = async (event) => {
     const hashedPassword = await bcrypt.hash(requestBody.password, HASH_SALT_ROUNDS);
 
     // Save the user to DynamoDB
-    await createUser({ email: requestBody.email, password: hashedPassword });
+    await createUser({ email: requestBody.email, password: hashedPassword,name: requestBody.name,address: requestBody.address});
 
     // Generate and sign a JWT token
-    const payload = { email: requestBody.email }; // Include any user data you want in the payload
+    const payload = { email: requestBody.email, password: hashedPassword,name: requestBody.name,address: requestBody.address}; // Include any user data you want in the payload
     const token = sign(payload, JWT_SECRET, { expiresIn: '1h' });
 
     return {
@@ -103,40 +89,14 @@ export const signup: APIGatewayProxyHandler = async (event) => {
 };
 
 
-
-// export const login: APIGatewayProxyHandler = async (event) => {
-//   try {
-//     const requestBody: LoginRequestBody = JSON.parse(event.body);
-
-//     // Find the user with the given email from DynamoDB
-//     const user = await getUserByEmail(requestBody.email);
-
-//     // Check if the user exists and compare the passwords
-//     if (!user || !(await bcrypt.compare(requestBody.password, user.password))) {
-//       return {
-//         statusCode: 401,
-//         body: JSON.stringify({ error: 'Invalid credentials.' }),
-//       };
-//     }
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify({ message: 'Login successful.' }),
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: 'Internal server error' }),
-//     };
-//   }
-// };
-
 export const login: APIGatewayProxyHandler = async (event) => {
   try {
     const requestBody: LoginRequestBody = JSON.parse(event.body);
 
     // Find the user with the given email from DynamoDB
     const user = await getUserByEmail(requestBody.email);
+    console.log("user is here", user);
+
 
     // Check if the user exists and compare the passwords
     if (!user || !(await bcrypt.compare(requestBody.password, user.password))) {
--- src/functions/auth/registerSchema.ts
+++ src/functions/auth/registerSchema.ts
@@ -2,9 +2,14 @@ export default {
     type: 'object',
     properties: {
       name: { type: 'string' },
+      address: { type: 'string' },
       email: { type: 'string', format: 'email' },
       password: { type: 'string', minLength: 8 },
     },
     required: ['name', 'email', 'password'],
   } as const;
   
+//   Name
+// - Email
+// - Phone
+// - Address
